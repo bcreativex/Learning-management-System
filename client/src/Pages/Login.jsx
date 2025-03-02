e525d45173182465edde357a9785151f314f62c4
krishna <krishnajatav504@gmail.com>
2025-03-02 14:13:43 +0530

    login page updated
    color theme

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -55,7 +55,7 @@ export default function Login() {
           noValidate
           className="flex flex-col dark:bg-base-100 gap-4 rounded-lg md:py-5 py-7 md:px-7 px-3 md:w-[500px] w-full shadow-custom dark:shadow-xl  "
         >
-          <h1 className="text-center dark:text-purple-500 text-4xl font-bold font-inter">
+          <h1 className="text-center dark:text-[#009fe8] text-4xl font-bold font-inter">
             Login Page
           </h1>
           {/* email */}
@@ -80,7 +80,7 @@ export default function Login() {
           {/* submit btn */}
           <button
             type="submit"
-            className="mt-2 bg-yellow-500 text-white dark:text-base-200  transition-all ease-in-out duration-300 rounded-md py-2 font-nunito-sans font-[500]  text-lg cursor-pointer"
+            className="mt-2 bg-[#009fe8] text-white dark:text-base-200  transition-all ease-in-out duration-300 rounded-md py-2 font-nunito-sans font-[500]  text-lg cursor-pointer"
             disabled={isLoading}
           >
             {isLoading ? "Logging..." : "Login"}
